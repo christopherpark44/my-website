366c2d2cf22467ae40e6f2dd41486de241075f1a
christopherpark88 <christopherpark88@gmail.com>
2020-07-01 14:07:17 -0400

    Updates

---
+++ precache-manifest.4c3bb384b012c9eb3df4d97bc650e51d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10676571b9061867b847c9b57138bc1f",
+    "url": "/my-website/index.html"
+  },
+  {
+    "revision": "791f2021a9a650fc3632",
+    "url": "/my-website/static/css/main.b758cbcf.chunk.css"
+  },
+  {
+    "revision": "5cebe94688db95b8e50a",
+    "url": "/my-website/static/js/2.c701dc15.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/my-website/static/js/2.c701dc15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "791f2021a9a650fc3632",
+    "url": "/my-website/static/js/main.bdc9e79e.chunk.js"
+  },
+  {
+    "revision": "dc2e3b3511453d8cdedf",
+    "url": "/my-website/static/js/runtime-main.dac0fb3d.js"
+  },
+  {
+    "revision": "c4d4aa3e62774f3521a85fab99960a4f",
+    "url": "/my-website/static/media/CIBCLogo.c4d4aa3e.png"
+  },
+  {
+    "revision": "f6108efd045ebcd489345bc22964bb86",
+    "url": "/my-website/static/media/ChaiLogo.f6108efd.png"
+  },
+  {
+    "revision": "3d380d66f1701ab615ea3c159a610456",
+    "url": "/my-website/static/media/JavaLogo.3d380d66.png"
+  },
+  {
+    "revision": "0cded3a3276425911d55a2552bf361bf",
+    "url": "/my-website/static/media/JavaScriptLogo.0cded3a3.png"
+  },
+  {
+    "revision": "9edcb5106c553737a172ec833810fe2e",
+    "url": "/my-website/static/media/JestLogo.9edcb510.png"
+  },
+  {
+    "revision": "97015d8f7ee3bfd6312ed84eb7ab3201",
+    "url": "/my-website/static/media/NodeJSLogo.97015d8f.png"
+  },
+  {
+    "revision": "067050f6ecc418ea7a7d2f3557683c8c",
+    "url": "/my-website/static/media/PythonLogo.067050f6.png"
+  },
+  {
+    "revision": "37fe8322b169ddbdeabf75930e886ac6",
+    "url": "/my-website/static/media/ReactJSLogo.37fe8322.png"
+  },
+  {
+    "revision": "d4366b66a526370e2862e0679a2f80e7",
+    "url": "/my-website/static/media/SetterLogo.d4366b66.png"
+  },
+  {
+    "revision": "4e96f1110cb872407e837d4fc1440f96",
+    "url": "/my-website/static/media/TypeScriptLogo.4e96f111.png"
+  },
+  {
+    "revision": "52ac0bec6bb7e309c2bdd57cdd800be2",
+    "url": "/my-website/static/media/resume.52ac0bec.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.66745cb17b87141ecde10e02bc14fdfe.js"
+  "/my-website/precache-manifest.4c3bb384b012c9eb3df4d97bc650e51d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-website/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
